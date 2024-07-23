d5426a8ebae91abe7810e537589e6cf7c74817cd
Stilo <81645252+emyst01@users.noreply.github.com>
2024-07-23 11:19:18 +0200

    Delete silamena-website directory

--- silamena-website/public/assets/scripts/back.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keydown', (e) => {
-    if(e.key == 'Escape') {
-        history.back();
-    }
-})
--- silamena-website/public/assets/scripts/roles.js
+++
@@ -1,5 +0,0 @@
-const roles = ["", "Noun", "Verb", "Number", "Conjuction", "Preposition", "Adverb", "Auxiliary", "Adjective", "Pronoun", "Interjection", "Other"];
-
-function decRole(n) {
-    return roles[n];
-}
